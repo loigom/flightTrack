d7d4bb04f5f8547b70d5a6a5640583bae6512b99
Tanel Loigom <tanelloigom@gmail.com>
2023-02-18 05:18:37 +0200

    progress on models, admin screen

--- itineraryStalk/base/admin.py
+++ itineraryStalk/base/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Itinerary
+from .models import Itinerary, ItineraryFlight, Airline
 
-admin.site.register(Itinerary)
+class ItineraryFlightAdmin(admin.ModelAdmin):
+    exclude = ItineraryFlight.ADMIN_EXCLUDED
+
+regular_models = (
+    Itinerary,
+    Airline
+)
+
+admin.site.register(regular_models)
+admin.site.register(ItineraryFlight, ItineraryFlightAdmin)
--- itineraryStalk/base/models.py
+++ itineraryStalk/base/models.py
@@ -1,7 +1,47 @@
 from django.db import models
 
+
 class Itinerary(models.Model):
     name = models.TextField()
 
     def __str__(self) -> str:
         return self.name
+
+class Airline(models.Model):
+    name = models.TextField()
+    icao = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class ItineraryFlight(models.Model):
+    ADMIN_EXCLUDED = (
+        "actual_takeoff_ts",
+        "actual_landed_ts",
+        "status"
+    )
+
+    itinerary = models.ForeignKey(Itinerary, on_delete=models.PROTECT)
+    airline = models.ForeignKey(Airline, on_delete=models.PROTECT)
+    number_in_itinerary = models.IntegerField()
+    plane_number = models.TextField()
+    estimated_takeoff = models.DateTimeField()
+    estimated_landing = models.DateTimeField()
+
+    # The below attributes are hidden from the admin page. Back-end modifying only.
+    actual_takeoff_ts = models.IntegerField(null=True, blank=True)
+    actual_landed_ts = models.IntegerField(null=True, blank=True)
+
+    NOT_DEPARTED = "NONE"
+    IN_FLIGHT = "FLYING"
+    LANDED = "LANDED"
+    STATUSES = (
+        (NOT_DEPARTED, "Not departed"),
+        (IN_FLIGHT, "Flying"),
+        (LANDED, "Landed")
+    )
+    status = models.TextField(choices=STATUSES, default=NOT_DEPARTED)
+
+
+    def __str__(self) -> str:
+        return f"[{self.itinerary}] {self.airline} {self.plane_number} ({self.number_in_itinerary})"
